14acb102d088440cfcb068bda51b2ef0e0dacbb7
Francogalfre <galfre.vn@gmail.com>
2022-02-22 19:48:28 -0300

    Crombie Project Page Responsive Complete

--- pages/project/crombie.jsx
+++ pages/project/crombie.jsx
@@ -4,12 +4,8 @@ import Head from "next/head";
 import { BsArrowLeftShort } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
-import { Footer } from '../../components/Footer'
-import {
-  SiNextdotjs,
-  SiReact,
-  SiTailwindcss,
-} from "react-icons/si";
+import { Footer } from "../../components/Footer";
+import { SiNextdotjs, SiReact, SiTailwindcss } from "react-icons/si";
 
 // Swiper Slider
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -84,24 +80,24 @@ function ProjectPage() {
       </Head>
 
       <div className="md:max-w-6xl md:mx-auto mt-36 px-6 pt-6">
-
-        <Link
-          href={`/works`}
-          passHref
-        >
+        <Link href={`/works`} passHref>
           <a className="group">
             <div className="flex items-center mb-6 ">
-              <BsArrowLeftShort className="duration-200 group-hover:transform group-hover:-translate-x-2" />
-              <p className="text-[#111010] ml-1 text-xs font-semibold">Return</p>
+              <BsArrowLeftShort className="duration-200 md:w-6 md:h-6 group-hover:transform group-hover:-translate-x-2" />
+              <p className="text-[#111010] ml-1 text-xs font-semibold md:text-[16px] ">
+                Return
+              </p>
             </div>
           </a>
         </Link>
 
-        <h1 className="text-[#111010] text-[20px] font-bold leading-[1.45] ">
+        <h1 className="text-[#111010] text-[20px] md:text-4xl font-bold leading-[1.45] ">
           Crombie NextJS + TailwindCSS Webpage UI Clone
         </h1>
-        <p className="text-[#777] mt-2 text-xs ">Galfré Valentín - ©2022</p>
-        <p className="text-[#777] text-xs ">
+        <p className="text-[#777] mt-2 text-xs md:text-[16px] md:mt-4 ">
+          Galfré Valentín - ©2022
+        </p>
+        <p className="text-[#777] text-xs md:text-[16px] md:mt-2">
           Take a look and discover how it works 🧪.
         </p>
 
@@ -121,10 +117,10 @@ function ProjectPage() {
             />
           </div>
 
-          <h1 className="text-[#111010] text-[20px] mt-12 font-bold leading-[1.45] ">
+          <h1 className="text-[#111010] md:text-3xl text-[20px] mt-12 md:mt-24 font-bold leading-[1.45] ">
             Overview
           </h1>
-          <p className="text-[#777] mt-2 text-xs leading-5">
+          <p className="text-[#777] mt-2 text-xs md:text-[16px] md:leading-6 leading-5">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
             architecto eum aperiam accusantium qui ipsam voluptates, unde
             voluptatibus temporibus at iure adipisci laudantium illum est
@@ -132,26 +128,31 @@ function ProjectPage() {
             commodi, veniam molestiae dignissimos laboriosam vel nemo!
           </p>
 
-          <p className="text-[#777] mt-8 text-xs leading-5">
+          <p className="text-[#777] mt-8 text-xs md:mt-16 leading-5 md:text-[16px] ">
             This project was build using 🔨
           </p>
 
           <div>
-            <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <div className="mt-10 ">
+              <dl className="space-y-6 md:space-y-14">
                 {features.map((feature) => (
-                  <div key={feature.name} className="relative">
+                  <div key={feature.name} className="group relative">
                     <dt>
-                      <div
-                        className={`${feature.bg} ${feature.text} absolute flex items-center justify-center h-12 w-12 rounded-md `}
-                      >
-                        <feature.icon className="h-6 w-6" aria-hidden="true" />
-                      </div>
-                      <p className="ml-16 text-[13px] leading-6 font-bold text-[#111010] ">
+                      <a href={feature.link} target="_blank" rel="noreferrer">
+                        <div
+                          className={`${feature.bg} ${feature.text} absolute flex items-center justify-center h-12 w-12  md:h-20 md:w-20 rounded-md `}
+                        >
+                          <feature.icon
+                            className="h-6 w-6 md:w-10 md:h-10"
+                            aria-hidden="true"
+                          />
+                        </div>
+                      </a>
+                      <p className="md:pt-1.5 ml-16 md:ml-24 md:text-xl text-[13px] leading-6 font-bold text-[#111010] ">
                         {feature.name}
                       </p>
                     </dt>
-                    <dd className="mt-2 ml-16 text-xs text-gray-500">
+                    <dd className="ml-16 md:ml-24 md:w-4/5 md:text-[16px] md:leading-5 text-xs text-[#777] ">
                       {feature.description}
                     </dd>
                   </div>
@@ -160,10 +161,10 @@ function ProjectPage() {
             </div>
           </div>
 
-          <h1 className="text-[#111010] text-[20px] mt-12 font-bold leading-[1.45] ">
+          <h1 className="text-[#111010] md:text-3xl md:mt-20 text-[20px] mt-12 font-bold leading-[1.45] ">
             Branding
           </h1>
-          <p className="text-[#777] mt-2 leading-5 text-xs ">
+          <p className="text-[#777] mt-2 md:text-[16px] md:leading-6 leading-5 text-xs ">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
             architecto eum aperiam accusantium qui ipsam voluptates, unde
             voluptatibus temporibus at iure adipisci laudantium illum est
@@ -172,10 +173,12 @@ function ProjectPage() {
           </p>
         </div>
 
-        <h1 className="text-[#111010] text-[20px] font-bold leading-[1.45] ">
+        <h1 className="text-[#111010] md:mt-20 md:text-3xl text-[20px] font-bold leading-[1.45] ">
           Galery
         </h1>
-        <p className="text-[#777] text-xs mb-5">Take a better look.</p>
+        <p className="text-[#777] md:text-[16px] text-xs md:mt-1 md:mb-8 mb-5">
+          Take a better look.
+        </p>
         <Swiper
           slidesPerView={2}
           spaceBetween={30}
@@ -184,50 +187,59 @@ function ProjectPage() {
             delay: 4000,
             disableOnInteraction: false,
           }}
+          breakpoints={{
+            560: {
+              slidesPerView: 3
+            },
+            802: {
+              slidesPerView: 4,
+            },
+          }}
           modules={[Autoplay, Mousewheel]}
         >
           {images.map((img, index) => (
             <SwiperSlide key={index}>
-              <div className="w-full relative h-80 border-[.5px] rounded-xl">
+              <div className="w-full md:h-[500px] relative h-80 border-[.5px] rounded-xl">
                 <Image
                   src={img.src}
                   alt={`Slide ${index}`}
                   layout="fill"
                   placeholder="blur"
                   blurDataURL={img.src}
+                  quality={100}
                   className="rounded-xl"
                 />
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
-      </div>
 
-      <p className="text-[#777] mt-12  text-xs px-6 mb-2">
-        Test it by yourself ✨
-      </p>
-      <div className="flex px-6 space-x-4 items-center ">
-        <Link
-          href={"https://github.com/galfrevn/nextjs-crombie-clone"}
-          passHref
-        >
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="text-[12px] bg-black font-semibold hover:bg-[#222] transitiona-all duration-300  text-white py-3 px-6 "
+        <p className="text-[#777] md:text-[16px] mt-12 md:mb-4 text-xs mb-2">
+          Test it by yourself ✨
+        </p>
+        <div className="flex space-x-4 items-center ">
+          <Link
+            href={"https://github.com/galfrevn/nextjs-crombie-clone"}
+            passHref
           >
-            Github repo
-          </a>
-        </Link>
-        <Link href={"https://vncrombie.netlify.app"} passHref>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="text-[12px] bg-[#f36] font-semibold transitiona-all duration-300 text-white py-3 px-6 "
-          >
-            Live demo
-          </a>
-        </Link>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="text-[12px] md:text-[16px] md:py-4 md:px-8 bg-black font-semibold hover:bg-[#222] transitiona-all duration-300  text-white py-3 px-6 "
+            >
+              Github repo
+            </a>
+          </Link>
+          <Link href={"https://vncrombie.netlify.app"} passHref>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="text-[12px] md:text-[16px] md:py-4 md:px-8 bg-[#f36] font-semibold transitiona-all duration-300 text-white py-3 px-6 "
+            >
+              Live demo
+            </a>
+          </Link>
+        </div>
       </div>
 
       <div className="pt-20 pb-4 ">
