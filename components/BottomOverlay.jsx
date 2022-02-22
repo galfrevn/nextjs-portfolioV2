3bcac5102bb65e265022e8af83ce6b4acb830637
Francogalfre <galfre.vn@gmail.com>
2022-02-22 15:52:57 -0300

    Contact page

--- components/BottomOverlay.jsx
+++ components/BottomOverlay.jsx
@@ -1,11 +1,14 @@
 import { ArrowCircleDownIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import { SiLinkedin, SiGithub } from 'react-icons/si'
-import { RiInstagramFill } from 'react-icons/ri'
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import { RiInstagramFill } from "react-icons/ri";
+import { useRouter } from "next/router";
 
 export const BottomOverlay = () => {
   const [scroll, setScroll] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -13,35 +16,45 @@ export const BottomOverlay = () => {
     });
   }, [scroll]);
 
+  useEffect(() => {
+    router.pathname === "/contact"
+      ? setVisible(() => false)
+      : setVisible(() => true);
+  }, [router.pathname]);
+
   return (
-    <div
-      className={
-        scroll
-          ? "fixed -bottom-20 transition-all duration-1000 left-0 backdrop-blur-sm bg-[rgba(255,255,255,.95)] py-2.5 md:py-4 px-7 w-full "
-          : "fixed transition-all duration-1000 bottom-0 left-0 backdrop-blur-sm bg-[rgba(255,255,255,.95)] py-2.5 md:py-4 px-7 w-full "
-      }
-    >
-      <div className="flex md:max-w-6xl md:mx-auto items-center md:justify-end justify-between">
-        <div className="flex items-center md:hidden ">
-          <ArrowCircleDownIcon className="w-5 h-5 text-[#111010] " />
-          <p className="text-xs ml-2 text-[#111010]">Scroll</p>
-        </div>
-        <Link href="/contact" passHref>
-          <a>
-            <h3 className="text-[11px] md:hidden text-[#111010] font-bold">
-              Let&apos;s talk
-            </h3>
-            <div className="hidden md:flex space-x-3 items-center pr-6">
-              <h3 className=" text-lg mr-3 text-[#111010] font-semibold">
-                Find me on
-              </h3>
-              <SiLinkedin className="w-6 h-6 text-[#302f2f]" />
-              <RiInstagramFill className="w-7 h-7 text-[#302f2f] " />
-              <SiGithub className="w-[26px] h-[26px] text-[#302f2f] " />
+    <>
+      {visible && (
+        <div
+          className={
+            scroll
+              ? "fixed -bottom-20 transition-all duration-1000 left-0 backdrop-blur-sm bg-[rgba(255,255,255,.95)] py-2.5 md:py-4 px-7 w-full "
+              : "fixed transition-all duration-1000 bottom-0 left-0 backdrop-blur-sm bg-[rgba(255,255,255,.95)] py-2.5 md:py-4 px-7 w-full "
+          }
+        >
+          <div className="flex md:max-w-6xl md:mx-auto items-center md:justify-end justify-between">
+            <div className="flex items-center md:hidden ">
+              <ArrowCircleDownIcon className="w-5 h-5 text-[#111010] " />
+              <p className="text-xs ml-2 text-[#111010]">Scroll</p>
             </div>
-          </a>
-        </Link>
-      </div>
-    </div>
+            <Link href="/contact" passHref>
+              <a>
+                <h3 className="text-[11px] md:hidden text-[#111010] font-bold">
+                  Let&apos;s talk
+                </h3>
+                <div className="hidden md:flex space-x-3 items-center pr-6">
+                  <h3 className=" text-lg mr-3 text-[#111010] font-semibold">
+                    Find me on
+                  </h3>
+                  <SiLinkedin className="w-6 h-6 text-[#302f2f]" />
+                  <RiInstagramFill className="w-7 h-7 text-[#302f2f] " />
+                  <SiGithub className="w-[26px] h-[26px] text-[#302f2f] " />
+                </div>
+              </a>
+            </Link>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,9 +1,46 @@
 import Head from "next/head";
 import React from "react";
 import { Footer } from "../components/Footer";
+import { SiLinkedin, SiGithub, SiGmail } from "react-icons/si";
+import { RiInstagramFill } from "react-icons/ri";
+
+const features = [
+  {
+    name: "LinkedIn",
+    description: "Discover what I have to offer in a more professional way",
+    icon: SiLinkedin,
+    bg: "bg-[#ecf4ff]",
+    text: "text-[#5393fe]",
+    link: "https://linkedin.com/in/galfrevn",
+  },
+  {
+    name: "Instagram",
+    description: "Know more about my personal life & hobbies",
+    icon: RiInstagramFill,
+    bg: "bg-[#ffefea]",
+    text: "text-[#f19e82]",
+    link: "https://instagram.com/galfre.v",
+  },
+  {
+    name: "Github",
+    description:
+      "Find out all my repositories and the code behind my projects!",
+    icon: SiGithub,
+    bg: "bg-[#f3ebff]",
+    text: "text-[#B57BFF]",
+    link: "https://github.com/galfrevn",
+  },
+  {
+    name: "Email",
+    description: "Send me your proposal at: galfre.vn@gmail.com",
+    icon: SiGmail,
+    bg: "bg-[#e1f8f8]",
+    text: "text-[#17B1B1]",
+    link: "mailto:galfre.vn@gmail.com",
+  },
+];
 
 function Contact() {
-
   return (
     <>
       <Head>
@@ -13,162 +50,38 @@ function Contact() {
         <meta name="theme-color" content="#FFF" />
       </Head>
 
-      <div className="md:max-w-2xl md:mx-auto mt-36 p-6">
-        <h1 className="text-[#111010] text-[28px] font-bold leading-[1.45] ">
+      <div className="md:max-w-6xl md:mx-auto mt-36 p-6">
+        <h1 className="text-[#111010] text-[28px] md:text-5xl font-bold leading-[1.45] ">
           Contact
         </h1>
-        <p className="text-[#777] text-xs ">
+        <p className="text-[#777] text-xs md:text-[16px] md:mt-2 ">
           Find All the ways to reach me here! 🍻
         </p>
 
-        <div className="mt-12 mb-20">
-          <form
-            action=""
-            className="text-[11px]"
-            autoComplete="off"
-          >
-            <div className="mb-6 ">
-              <p className=" text-[#111010] text-xl font-bold mb-2">
-                I&apos;m looking to
-              </p>
-
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Create a project from scratch.
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Continue / improve an existing project.
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Hire Consultancy Services.
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Just need cost/time estimate guidance
-              </label>
-            </div>
-
-            <div className="mb-6 ">
-              <p className=" text-[#111010] text-xl font-bold mb-2">
-                I want to hire
-              </p>
-
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Custom Web Development Product
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                Custom Mobile Development Product
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                CMS/E-Commerce
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                A team for Staff Augmentation
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                I don&apos;t know
-              </label>
-            </div>
-
-            <div className="mb-6 ">
-              <p className=" text-[#111010] text-xl font-bold mb-2">
-                I already have
-              </p>
-
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] bg-white rounded-sm checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                A detailed brief to share.
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                I only have an idea and would like some help with it.
-              </label>
-              <label className="text-[#111010] text-[9px] flex items-center mb-2">
-                <input
-                  className="form-check-input appearance-none transition-all h-3 w-3 border border-[#777] rounded-sm bg-white checked:bg-black checked:border-white focus:outline-none duration-200 bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                I don&apos;t have anything
-              </label>
-
-              <div className="overflow-hidden relative transform mt-4 w-1/2 flex justify-start items-start">
-                <button className="bg-indigo hover:bg-indigo-dark text-white text-[9px] border-white rounded-sm border-[1px] py-1 px-4 inline-flex items-center">
-                  <p className="">Attach your brief</p>
-                </button>
-                <input
-                  className="cursor-pointer absolute block opacity-0 pin-r pin-t"
-                  type="file"
-                  name="vacancyImageFiles"
-                />
-              </div>
-            </div>
-
-            
-          </form>
+        <div className="mt-6 md:mt-12 mb-12 md:mb-20">
+          <div className="mt-10">
+            <dl className="space-y-6 md:space-y-14">
+              {features.map((feature) => (
+                <div key={feature.name} className="group relative">
+                  <dt>
+                    <a href={feature.link} target="_blank" rel="noreferrer"  >
+                      <div
+                        className={`${feature.bg} ${feature.text} absolute flex items-center justify-center h-12 w-12  md:h-20 md:w-20 rounded-md `}
+                      >
+                        <feature.icon className="h-6 w-6 md:w-10 md:h-10" aria-hidden="true" />
+                      </div>
+                    </a>
+                    <p className="md:pt-4 ml-16 md:ml-24 md:text-xl text-[13px] leading-6 font-bold text-[#111010] ">
+                      {feature.name}
+                    </p>
+                  </dt>
+                  <dd className="ml-16 md:ml-24 md:text-[16px] text-xs text-[#777] ">
+                    {feature.description}
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
       </div>
       <footer className="pb-4 ">
