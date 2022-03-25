d0f01515ee9fd687fc88efd07fb0730750bda732
Francogalfre <galfre.vn@gmail.com>
2022-03-24 21:19:11 -0300

    PWA Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
-  images: {
-    domains: ["crombie.dev", "images.prismic.io", "upload.wikimedia.org", 'media2.giphy.com'],
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
   },
-};
+  images: {
+    domains: [
+      "crombie.dev",
+      "images.prismic.io",
+      "upload.wikimedia.org",
+      "media2.giphy.com",
+    ],
+  },
+});
